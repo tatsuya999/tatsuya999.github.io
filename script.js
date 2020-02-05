8fc0255c65c75ad2d4c5a8f92485f99c5e279bb3
tatsuya_ego <ts2000kasa@gmail.com>
2020-02-05 14:35:10 +0900

    done

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".loading").ripples({
+  dropRadius: 20,
+  perturbance: 0.01,
+  resolution: 170
+});
+
+$(".loading").ripples(
+  "drop",
+  screen.availWidth / 2,
+  screen.availHeight / 2,
+  200,
+  10
+);
+
+$("body").ripples({
+  resolution: 300,
+  dropRadius: 35,
+  perturbance: 0.1
+});
+
+$(".sidebar").ripples({
+  resolution: 300,
+  dropRadius: 35,
+  perturbance: 0.1
+});
+
+$(".main").ripples({
+  imageUrl: "name-momonga.png"
+});
+$(".wrap").ripples(
+  "drop",
+  $(".wrap").width() - 64,
+
+  $(".wrap").height() - 64,
+  2,
+  10
+);
+
+$(".profile").ripples({
+  resolution: 300,
+  dropRadius: 35,
+  perturbance: 0.1
+});
+
+$(".profile--image").ripples({
+  resolution: 300,
+  dropRadius: 35,
+  perturbance: 0.1
+});
