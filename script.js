8bd5283302bdb648f33acc99c5d3f1a5f5fe8cb9
tatsuya_ego <ts2000kasa@gmail.com>
2023-03-30 19:36:13 +0900

    反射ループ カーソル処理

--- script.js
+++ script.js
@@ -44,25 +44,51 @@ $(".profile--image").ripples({
 });
 
 //ホーム画面の選択オブジェクトの動き
-function moveObject(object_id){
-  object_id.style.animate({
-    left:['0px','200px'],
-   }
-  );
+function moveObject(){
+  //windowの開始時間
+  let prev_time = new Date();
+  //vx オブジェクトの速度
+  let vx = 10;
+  // vector オブジェクトの方向
+  let vector = 1;
+  // querySelector オブジェクトのid取得
+  const object = document.querySelector('#link_profile');
+  // movePositionX オブジェクトのx座標
+  let movePositionx= 0;
+    //アニメーションループの内容
+    const Animation=()=>{
+      console.log("start Animation",object.style.left)
+      const now = new Date();
+      const diffSecond = (now.getTime() - prev_time.getTime()) * 0.01;
+      prev_time = now;
+      movePositionx = movePositionx + (vx*diffSecond);
+      object.style.left= movePositionx+'px';
+      //画面端でバウンド処理
+      if(parseInt(object.style.left,10) >= screen.availWidth-63 || parseInt(object.style.left,10)<=-1){
+        vx = -vx;
+        vector = -vector;
+      }
+      //'mouseover'でカーソルがオブジェクトに当たった時停止、'mouseout'でカーソルが外れた時再稼働
+      object.addEventListener('mouseover',function(){
+        vx = 0;
+      })
+      object.addEventListener('mouseout',function(){
+        vx = 10*vector;
+      })
+      //アニメーションのループ
+      requestAnimationFrame(Animation);
+    }
+    //アニメーション開始
+    Animation();
 }
 
 function reflectionObject(){
-
-}
-
-window.onload = function(){
   let start = Date.now();
   let timer = setInterval(function() {
     let timePassed = Date.now() - start;
     v = 1;
     link_profile.style.left = (v*timePassed/5) + 'px';
-
-    if (parseInt(link_profile.style.left,10) >= screen.availWidth-60){
+    if(parseInt(link_profile.style.left,10) >= screen.availWidth-60){
     v = -v;
     link_profile.style.left =(screen.availWidth-60)*2+(v*timePassed/5) + 'px';
     }
@@ -70,10 +96,16 @@ window.onload = function(){
     v= -v;
     link_profile.style.left = (v*timePassed/5)-(screen.availWidth-60)*2 + 'px';
     }
+    if(parseInt(link_profile.style.left,10) >= screen.availWidth-60){
+      v = -v;
+      link_profile.style.left =(screen.availWidth-60)*4+(v*timePassed/5) + 'px';
+      }
     //止める関数
     //clearInterval(timer);
-    console.log(v,parseInt(link_profile.style.left,10))
+    console.log(v,parseInt(link_profile.style.left,10),screen.availWidth)
     
   }, 20);
-};
+}
+
+window.addEventListener('load' ,moveObject());
 //moveObject(".link_profile");
