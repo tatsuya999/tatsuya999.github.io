daa7a8a228a400fc0ae11f0b5daeaf90daba629d
tatsuya_ego <ts2000kasa@gmail.com>
2023-04-03 00:45:04 +0900

    add 4 object

--- script.js
+++ script.js
@@ -44,9 +44,10 @@ function moveObject(id){
   // オブジェクトの大きさ
   let object_size_width = object.clientWidth;
   let object_size_hight = object.clientHeight;
+  console.log(object_size_hight,object_size_width)
   // movePositionX オブジェクトのx座標
-  let movePositionx= getRandomArbitrary(5,window.innerWidth-70);
-  let movePositiony= getRandomArbitrary(0,window.innerHeight-60);
+  let movePositionx= getRandomArbitrary(5,window.innerWidth-100);
+  let movePositiony= getRandomArbitrary(0,window.innerHeight-100);
   //オブジェクト移動速度減衰率
   let ref = 0.998;
   //波紋発火頻度パラメータ
@@ -67,11 +68,13 @@ function moveObject(id){
       if(Math.min(parseInt(object.style.left,10)+object_size_width) >= window.innerWidth || Math.min(parseInt(object.style.left,10))<=0||objectCollision(id)){
         vx = -vx;
         vector_x = -vector_x;
+        objectRipples(movePositionx,movePositiony)
       }
       //画面端(height)と衝突でバウンド処理
       if(Math.min(parseInt(object.style.top,10)+object_size_hight) >= window.innerHeight|| Math.min(parseInt(object.style.top,10))<=0||objectCollision(id)){
         vy = -vy;
         vector_y = -vector_y;
+        objectRipples(movePositionx,movePositiony)
       }
       //減衰によるオブジェクトの停止
       if(Math.abs(Math.sqrt((vx**2)*(vy**2)))<0.5){
@@ -110,8 +113,8 @@ function objectRipples(movePositionx,movePositiony){
   //波紋のエフェクト
   $(".main").ripples(
     "drop",
-    movePositionx+32,
-    movePositiony+32,
+    movePositionx+64,
+    movePositiony+64,
     50,
     0.1,
   );
@@ -125,7 +128,7 @@ function dist(x1,y1,x2,y2){
   let len = Math.abs(Math.sqrt((x1-x2)**2+(y1-y2)**2));
   return len;
 }
-
+//オブジェクトの衝突制御
 function objectCollision(object_id){
   object_list = document.getElementsByClassName('links');
   // オブジェクトの中心
@@ -133,9 +136,44 @@ function objectCollision(object_id){
   let object_center0_y = (parseInt(object_list[0].style.top,10)+object_list[0].clientHeight/2);
   let object_center1_x = (parseInt(object_list[1].style.left,10)+object_list[1].clientWidth/2);
   let object_center1_y = (parseInt(object_list[1].style.top,10)+object_list[1].clientHeight/2);
+  let object_center2_x = (parseInt(object_list[2].style.left,10)+object_list[2].clientWidth/2);
+  let object_center2_y = (parseInt(object_list[2].style.top,10)+object_list[2].clientHeight/2);
+  let object_center3_x = (parseInt(object_list[3].style.left,10)+object_list[3].clientWidth/2);
+  let object_center3_y = (parseInt(object_list[3].style.top,10)+object_list[3].clientHeight/2);
+  //それぞれの距離
   let d_01 = dist(object_center0_x,object_center0_y,object_center1_x,object_center1_y);
-  if(d_01<(object_list[0].clientWidth/2)+(object_list[1].clientWidth/2)){
+  let d_02 = dist(object_center0_x,object_center0_y,object_center2_x,object_center2_y);
+  let d_03 = dist(object_center0_x,object_center0_y,object_center3_x,object_center3_y);
+  let d_12 = dist(object_center2_x,object_center2_y,object_center1_x,object_center1_y);
+  let d_13 = dist(object_center3_x,object_center3_y,object_center1_x,object_center1_y);
+  let d_23 = dist(object_center2_x,object_center2_y,object_center3_x,object_center3_y);
+  if(d_01<=(object_list[0].clientWidth/2)+(object_list[1].clientWidth/2)){
     if(object_id==object_list[0].id||object_id==object_list[1].id){
+      return true;
+    }
+  }
+  if(d_02<=(object_list[0].clientWidth/2)+(object_list[2].clientWidth/2)){
+    if(object_id==object_list[0].id||object_id==object_list[2].id){
+      return true;
+    }
+  }
+  if(d_03<=(object_list[0].clientWidth/2)+(object_list[3].clientWidth/2)){
+    if(object_id==object_list[0].id||object_id==object_list[3].id){
+      return true;
+    }
+  }
+  if(d_12<=(object_list[1].clientWidth/2)+(object_list[2].clientWidth/2)){
+    if(object_id==object_list[1].id||object_id==object_list[2].id){
+      return true;
+    }
+  }
+  if(d_13<=(object_list[1].clientWidth/2)+(object_list[3].clientWidth/2)){
+    if(object_id==object_list[1].id||object_id==object_list[3].id){
+      return true;
+    }
+  }
+  if(d_23<=(object_list[2].clientWidth/2)+(object_list[3].clientWidth/2)){
+    if(object_id==object_list[2].id||object_id==object_list[3].id){
       return true;
     }
   }
@@ -144,5 +182,6 @@ function objectCollision(object_id){
 
 window.addEventListener('load' ,moveObject("link_works"));
 window.addEventListener('load' ,moveObject("link_profile"));
-window.addEventListener('load' ,objectCollision);
+window.addEventListener('load' ,moveObject("link_inst"));
+window.addEventListener('load' ,moveObject("link_music"));
 //moveObject(".link_profile");
